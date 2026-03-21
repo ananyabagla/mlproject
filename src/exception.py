d745db1f834b8cda67eec4a1af6b8c66e62482d0
ananyabagla <ananyabagla584@gmail.com>
2026-03-21 18:40:19 +0530

    Added data ingestion pipeline

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,10 @@
-import logging
 import sys
+
+try:
+    from logger import logging
+except ModuleNotFoundError:
+    from src.logger import logging
+
 
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
@@ -8,6 +13,7 @@ def error_message_detail(error, error_detail: sys):
     error_message = f"Error occurred in file: {file_name} at line number: {line_number} with error message: {str(error)}"
     return error_message
 
+
 class CustomException(Exception):
     def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
@@ -15,4 +21,12 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
+
+
+if __name__ == "__main__":
+    try:
+        a = 1 / 0
+    except Exception as e:
+        logging.info("Division by zero error occurred")
+        raise CustomException(e, sys)
     
